afcfd5e3a42aaa6e32adde0c55bb04a83228b05a
maybesomesense <228cacka228@mail.ru>
2023-10-29 17:37:36 +0300

    next version without map

--- stringLab.scala
+++ stringLab.scala
@@ -26,21 +26,46 @@ object stringLab {
 
       println(s"Суммарное число гласных в тексте: $vowelCount")
     } else if (choice == 2) {
-      val text = "Hello to everybody"
-      val immutableMap = Map('H' -> 'Н',
-                             'e' -> 'е',
-                             'l' -> 'л',
-                             'o' -> 'о',
-                             't' -> 'т',
-                             'v' -> 'в',
-                             'r' -> 'р',
-                             'y' -> 'у',
-                             'b' -> 'б',
-                             'd' -> 'д'
-                            )
-      val replaced = text.map(char => immutableMap.getOrElse(char.toLower, char))
+//      val text = "Hello to everybody"
+//      val immutableMap = Map('H' -> 'Н',
+//                             'e' -> 'е',
+//                             'l' -> 'л',
+//                             'o' -> 'о',
+//                             't' -> 'т',
+//                             'v' -> 'в',
+//                             'r' -> 'р',
+//                             'y' -> 'у',
+//                             'b' -> 'б',
+//                             'd' -> 'д'
+//                            )
+//      val replaced = text.map(char => immutableMap.getOrElse(char.toLower, char))
+//
+//      println(replaced)
 
-      println(replaced)
+      val text = "Hello to everybody"
+
+      val latinRegex = new Regex("[a-zA-Z]")
+
+      def replaceLatinWithRussian(matched: Regex.Match): String = {
+        val latinChar = matched.group(0)
+        val russianChar = latinChar match {
+          case "H" => "Н"
+          case "e" => "е"
+          case "l" => "л"
+          case "o" => "о"
+          case "t" => "т"
+          case "v" => "в"
+          case "y" => "у"
+          case "b" => "б"
+          case _ => latinChar
+        }
+        russianChar
+      }
+
+      val result = latinRegex.replaceAllIn(text, replaceLatinWithRussian _)
+
+      println(result)
+
     } else if (choice == 3) {
       val text = "When executing the exercise extract all extra words"
 
@@ -50,21 +75,27 @@ object stringLab {
 
       matches.foreach(println)
     } else if (choice == 4) {
+//      val text = "A big round ball fall to the ground"
+//      val words = text.split("\\s+")
+//
+//      def replaceTheWithA(word: String): String = {
+//        if (word.toLowerCase == "the") {
+//          "a"
+//        } else {
+//          word
+//        }
+//      }
+//
+//      val replacedText = words.map(replaceTheWithA).mkString(" ")
+//
+//      println(replacedText)
       val text = "A big round ball fall to the ground"
 
-      val words = text.split("\\s+")
+      val regex = "\\bthe\\b".r
 
-      def replaceTheWithA(word: String): String = {
-        if (word.toLowerCase == "the") {
-          "a"
-        } else {
-          word
-        }
-      }
+      val result = regex.replaceAllIn(text, _ => "a")
 
-      val replacedText = words.map(replaceTheWithA).mkString(" ")
-
-      println(replacedText)
+      println(result)
     } else if (choice == 5) {
       val text = "A big round ball falls to the ground"
 
@@ -79,23 +110,35 @@ object stringLab {
       val text = "Hello to everybody"
 
       def removeVowels(input: String): String = {
-        input.replaceAll("[aeiouAEIOU]", "")
+        input.replaceAll("[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]", "")
       }
 
       val textWithoutVowels = removeVowels(text)
 
       println(textWithoutVowels)
     } else if (choice == 7) {
+//      val text = "Hello to everybody"
+//
+//      def doubleLettersInWord(word: String): String = {
+//        word.flatMap(char => char.toString * 2)
+//      }
+//
+//      val words = text.split("\\s+")
+//      val textWithDoubledLetters = words.map(doubleLettersInWord).mkString(" ")
+//
+//      println(textWithDoubledLetters)
       val text = "Hello to everybody"
 
-      def doubleLettersInWord(word: String): String = {
-        word.flatMap(char => char.toString * 2)
+      val regex = "\\w".r
+
+      def doubleLetter(matched: Regex.Match): String = {
+        val letter = matched.group(0)
+        letter + letter
       }
 
-      val words = text.split("\\s+")
-      val textWithDoubledLetters = words.map(doubleLettersInWord).mkString(" ")
+      val result = regex.replaceAllIn(text, doubleLetter _)
 
-      println(textWithDoubledLetters)
+      println(result)
     } else if(choice == 8) {
       val text = "Hello to everybody"
 
@@ -118,7 +161,39 @@ object stringLab {
       val modifiedText = modifiedWords.mkString(" ")
 
       println(modifiedText)
+    } else if(choice == 10) {
+      val text = "Hello to everybody"
+      val pattern = new Regex("\\b\\w")
+      val modifiedString = pattern.replaceAllIn(text, m => m.matched * 3)
+
+      print(modifiedString)
+
+    } else if(choice == 11) {
+      val inputString = "Hello to everybody"
+      val outputString = inputString.replaceAll("\\s+", "")
+
+      println(outputString)
+
     }
+
 
   }
 }
+
+object RegexExample extends App {
+  // Исходный текст
+  val text = "This is a sample text with some words to extract."
+
+  // Регулярное выражение для поиска слов
+  val regex = "\\b\\w+\\b".r
+
+  // Находим все совпадения в тексте
+  val words = regex.findAllIn(text).toList
+
+  // Выводим найденные слова
+  words.foreach(println)
+}
+
+
+// предложение
+// каждое слово в предложении первую букву утроить
